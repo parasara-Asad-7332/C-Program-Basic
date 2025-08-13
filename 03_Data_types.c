0d221322ca0e57cfcc093bbd654907c32dbb9c83
parasara-Asad-7332 <kingofparasara786@gmail.com>
2025-08-13 17:42:18 +0530

    Add files via upload

---
+++ 03_Data_types.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a = 6; // store number
+    // int a ;
+    // a = 6 ; same as up
+
+    float b; // store float number like 3.5 , 4.9 . 5.4
+    b = 95.5;
+    // float b = 95.5; same as up
+
+    char c = 'A'; // Store a character
+    // char c;
+    // c = 'a'; // same as up
+
+    printf("this viule a %d\n", a);
+    printf("this viule b %f\n", b);
+    printf("this viule c %c\n", c);
+}
