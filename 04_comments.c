012837fdf80690c8584a74730ad358e73a353d43
parasara-Asad-7332 <kingofparasara786@gmail.com>
2025-08-14 18:36:20 +0530

    Add files via upload

---
+++ 04_comments.c
@@ -0,0 +1,13 @@
+/* this is a comments 
+malitiline */  //this is Multiline comments
+
+
+#include <stdio.h>
+
+int main(){
+    // printf("Hello world!"); now code not run Because // comments
+    
+    return 0;
+}
+   
+   
