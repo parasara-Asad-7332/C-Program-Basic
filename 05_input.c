860cac439aaf11b62a9954be200bbbdd1c117589
parasara-Asad-7332 <kingofparasara786@gmail.com>
2025-08-16 08:23:30 +0530

    Update 05_input.c

--- 05_input.c
+++ 05_input.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 int main(){
+    //Number user input
     int a; 
-    scanf("%d", &a);
-    printf("The value of a is %d", a);
+    printf("Enter a is: ");
+    scanf("%d", &a); //scanf is user input
+    printf("The value of a is %d\n", a);
+    //float user input
+    float b;
+    printf("Enter b is: ");
+    scanf("%f",&b);
+    printf("The value of b is: %f\n",b);
+    // Character user input
+    char c;
+    printf("Enter c is: ");
+    scanf(" %c",&c);
+    printf("The value of b is: %c",c);
+
     return 0;
 }
-
